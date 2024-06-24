052c103ca581fcb064e34e1deeedb3526f41112b
Shuvo-Khan <shuvo3846@gmail.com>
2024-06-24 15:36:48 +0600

    Maps Length

---
+++ .idea/Files/Practice2.dart
@@ -0,0 +1,12 @@
+void main(){
+  print('Hello world');
+
+  Map<int, String> map = {1 : 'Sajib',
+  2 : 'Raju',
+  4 : 'Pavel',
+  5 : 'Koly'};
+
+  for(int i = 1; i <= map.length; i++){
+    print(map[i]);
+  }
+}
